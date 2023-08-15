9d1cc1cf8e915d4a50d3ca49d45e8418b562c3a2
WDNa04 <WNa0531@outlook.com>
2023-08-15 12:52:40 +0800

    hi

--- test_LeetCode.py
+++ test_LeetCode.py
@@ -1,10 +1,18 @@
 import sys
+def addTwoNumbers(self,l1,l2):
+    l1 = l1[::-1]
+    l2 = l2[::-1]
+    linea = ''
+    lineb = ''
+    for i in l1:
+        linea += str(i)
+    for i in l2:
+        lineb += str(i)
+    number = int(linea) + int(lineb)
+    lista = list(reversed(list(str(number))))
+    return lista
 
-def findKthLargest(nums, k):
-    nums_list = list(map(int,nums.split(',')))
-    nums_list.remove(max(nums_list))
-    return max(nums_list)
+l1 = list(map(int,sys.stdin.readline().split(',')))
+l2 = list(map(int,sys.stdin.readline().split(',')))
+print(addTwoNumbers('self',l1,l2))
 
-nums = sys.stdin.readline()
-k = int(sys.stdin.readline())
-print(findKthLargest(nums, k))
